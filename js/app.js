188ad8c23fca69cc874ef4f53639d95b98895928
Claudio Acioli <claudio.acioli.silva@gmail.com>
2020-06-11 10:21:43 -0300

    feat: Add navbarlinks base to section

--- js/app.js
+++ js/app.js
@@ -13,19 +13,52 @@
  * 
 */
 
-/**
- * Define Global Variables
- * 
-*/
-
+document.addEventListener("DOMContentLoaded", e => {
 
 /**
- * End Global Variables
- * Start Helper Functions
- * 
+* Define Global Variables
+* 
 */
 
+  const 
+    navBarElement = byId("navbar__list"),
+    sectionElements = byAll("main section")
+  ;
 
+/**
+* End Global Variables
+* Start Helper Functions
+* 
+*/
+
+  const 
+
+    renderNavLink = ({href, text}) => {
+      const element = document.createElement("a");
+      element.setAttribute("href", href);
+      element.textContent = text;
+      return element;
+    },
+
+    renderNavItem = attr => {
+      const element = document.createElement("li");
+      element.appendChild(renderNavLink(attr));
+      return element;
+    },
+
+    renderNavBar = () => {
+      const fragment = document.createDocumentFragment();
+      sectionElements.forEach(element =>
+        fragment.appendChild(
+          renderNavItem({
+            "href": `#${element.id}`,
+            "text": element.dataset.nav
+          })
+        )
+      );
+      navBarElement.appendChild(fragment);
+    }
+  ;
 
 /**
  * End Helper Functions
@@ -49,9 +82,11 @@
 */
 
 // Build menu 
+  
+  renderNavBar();
 
 // Scroll to section on link click
 
 // Set sections as active
 
-
+});
